e388648cb26a0384e4a381f4bfc7bf66213ddbd7
Michael <z28blackpearl@gmail.com>
2023-01-31 03:35:27 +0000

    Starting to update the two server client

--- Async/async-client-3.py
+++ Async/async-client-3.py
@@ -1,28 +1,45 @@
 import errno
 import select
 import socket
+import time
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("localhost", 1234))
-sock.setblocking(False)
+def boringTask():
+    i = 0
+    while i < 20:
+        i += 1
+        print(i)
+        time.sleep(0.02)
+        yield
 
-data = "<(0_0<) <(0_0)> (>0_0)>".encode("utf8") * 10 * 1024 * 35  # Data to send
-dataSize = len(data)
-print("Bytes to send: %s" %(dataSize))
+def sendDataTask(port, data)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("localhost", port))
+    sock.setblocking(False)
 
-totalSent = 0
-while len(data):
-    try:
-        sent = sock.send(data)
-        totalSent += sent
-        data = data[sent:]
-        print("sending data")
-    except socket.error as e:
-        if e.errno != errno.EAGAIN:
-            raise e
-        print("Blocking with %s data remaining" % (len(data)))
-        select.select([], [sock], [])  # Blocking until all data sent
+    data = data.encode("utf8") * 10 * 1024 * 50
+    dataSize = len(data)
+    print("Bytes to send: %s" %(dataSize))
 
-assert sock.send(data) == len(data)  # Throws error if all data hasn't been sent
+    totalSent = 0
+    while len(data):
+        try:
+            sent = sock.send(data)
+            totalSent += sent
+            data = data[sent:]
+            print("sending data")
+        except socket.error as e:
+            if e.errno != errno.EAGAIN:
+                raise e
+            yield('w', sock)
+    
+    print('Bytes sent: %s' % totalSent)
 
-print("Finished")
+
+if __name__ == '__main__':
+    tasks = [
+        boringTask(), 
+        sendDataTask(port=1234, data='<(0_0<) <(0_0)> (>0_0)>'),
+        sendDataTask(port=5678, data='foobar')
+    ]
+
+    
