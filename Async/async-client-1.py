fa6498e68797e64af71e41a9423b831a981af27c
Michael <z28blackpearl@gmail.com>
2023-02-01 04:02:36 +0000

    change blocking to false

--- Async/async-client-1.py
+++ Async/async-client-1.py
@@ -4,7 +4,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("localhost", 1234))
 
 # Step 1 - Add line to prevent blocking by send
-sock.setblocking(0)
+sock.setblocking(False)
 
 data = "<(0_0<) <(0_0)> (>0_0)>".encode("utf8") * 10 * 1024 * 3  # Semi-large amount of data
 assert sock.send(data) == len(data)  # Throws error if all data hasn't been sent
