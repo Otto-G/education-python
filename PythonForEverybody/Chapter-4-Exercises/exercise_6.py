b32587eacbb33393d6d44e80b61dbdb428700882
Michael <z28blackpearl@gmail.com>
2023-03-14 00:05:35 +0000

    Wrong base code

--- PythonForEverybody/Chapter-4-Exercises/exercise_6.py
+++ PythonForEverybody/Chapter-4-Exercises/exercise_6.py
@@ -1,33 +1,23 @@
 "Chapter 4: Exercise 6"
 LIMIT = 5
-LEAVE = False
 for attempt in range(LIMIT):
-    while True:
-        try:
-            user_entered = input("Enter score (between 0 and 1): ")
-            if isinstance(user_entered, str) and user_entered.lower() == "q":
-                LEAVE = True
-                break
-            score = float(user_entered)
-        except ValueError:
-            print(f"Bad input {LIMIT - attempt - 1} tries left")
-            break
+    tries_left = LIMIT - attempt - 1
+    try:
+        hours = int(input("Enter Hours: "))
+    except ValueError:
+        print(f"Error, please enter numeric (int) input. {tries_left} tries left.")
+        continue
+    try:
+        pay = float(input("Enter Rate: "))
+    except ValueError:
+        print(f"Error, please enter numeric (float) input. {tries_left} tries left.")
+        continue
 
-        if score > 1.0:
-            print(f"Bad input {LIMIT - attempt - 1} tries left")
-            break
-        if score >= 0.9:
-            GRADE = "A"
-        elif score >= 0.8:
-            GRADE = "B"
-        elif score >= 0.7:
-            GRADE = "C"
-        elif score >= 0.6:
-            GRADE = "D"
-        else:
-            GRADE = "F"
+    total_pay = hours * pay
 
-        print(GRADE)
+    if hours > 40:
+        hours -= 40
+        total_pay += hours * pay * 0.5
 
-    if LEAVE is True:
-        break
+    print(f"Pay: ${total_pay:.2f}")
+    break
